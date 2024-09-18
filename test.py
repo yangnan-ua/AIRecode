1b412ed9ef5923f06c2c96d13b3f64b43e8da8f4
Yang <yangnan@arizona.edu>
2024-09-17 22:08:37 -0700

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ model = ResNet18() # 得到预训练模型
 model.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
 model.fc = torch.nn.Linear(512, n_class) # 将最后的全连接层修改
 # 载入权重
-model.load_state_dict(torch.load('/mnt/home/xuhan1/yangnan/AIRecode/resnet18_on_imbalanced_dataset/checkpoint/resnet18_cifar10_balance.pt'))
+model.load_state_dict(torch.load('AIRecode/resnet18_on_imbalanced_dataset/checkpoint/resnet18_cifar10_imbalance_reweighting_loss.pt'))
 model = model.to(device)
 
 total_sample = 0
@@ -47,6 +47,6 @@ for data, target in test_loader:
 accuracy_per_class = [100 * correct / total for correct, total in zip(correct_per_class, total_per_class)]
 
 # 打印每个类别的准确率
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')  # CIFAR-10 类别名称
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck') 
 for i, class_name in enumerate(classes):
     print(f"Accuracy of {class_name}: {accuracy_per_class[i]:.2f}%")
