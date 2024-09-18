59fffb161d8722f002b50a7f446d3bfdc0f4100e
Yang <yangnan@arizona.edu>
2024-09-17 21:59:07 -0700

    Add files via upload

---
+++ imbalance_cifar.py
@@ -0,0 +1,111 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from collections import Counter
+from torch.utils.data.sampler import SubsetRandomSampler
+
+
+class IMBALANCECIFAR10(torchvision.datasets.CIFAR10):
+    cls_num = 10
+
+    def __init__(self, root, imb_type='exp', imb_factor=0.01, rand_number=0, train=True,
+                 transform=None, target_transform=None,
+                 download=False):
+        super(IMBALANCECIFAR10, self).__init__(root, train, transform, target_transform, download)
+        np.random.seed(rand_number)
+        img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
+        self.gen_imbalanced_data(img_num_list)
+
+    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+        img_max = len(self.data) / cls_num
+        img_num_per_cls = []
+        if imb_type == 'exp':
+            for cls_idx in range(cls_num):
+                num = img_max * (imb_factor ** (cls_idx / (cls_num - 1.0)))
+                img_num_per_cls.append(int(num))
+        elif imb_type == 'step':
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max))
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max * imb_factor))
+        else:
+            img_num_per_cls.extend([int(img_max)] * cls_num)
+        return img_num_per_cls
+
+    def gen_imbalanced_data(self, img_num_per_cls):
+        new_data = []
+        new_targets = []
+        targets_np = np.array(self.targets, dtype=np.int64)
+        classes = np.unique(targets_np)
+        # np.random.shuffle(classes)
+        self.num_per_cls_dict = dict()
+        for the_class, the_img_num in zip(classes, img_num_per_cls):
+            self.num_per_cls_dict[the_class] = the_img_num
+            idx = np.where(targets_np == the_class)[0]
+            np.random.shuffle(idx)
+            selec_idx = idx[:the_img_num]
+            new_data.append(self.data[selec_idx, ...])
+            new_targets.extend([the_class, ] * the_img_num)
+        new_data = np.vstack(new_data)
+        self.data = new_data
+        self.targets = new_targets
+
+    def get_cls_num_list(self):
+        cls_num_list = []
+        for i in range(self.cls_num):
+            cls_num_list.append(self.num_per_cls_dict[i])
+        return cls_num_list
+
+def check_imbalance(dataset):
+    """
+    检查数据集是否为不平衡的，通过统计每个类别的样本数量
+    """
+    targets = dataset.targets  # 获取数据集中的标签列表
+    counter = Counter(targets)  # 统计每个类别的样本数量
+
+    print("类别样本分布：")
+    for cls, num in counter.items():
+        print(f"类别 {cls}: {num} 张图片")
+    
+    return counter  # 返回类别分布，便于进一步分析
+
+def read_dataset(batch_size=16,valid_size=0.2,num_workers=0):
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    train_data = IMBALANCECIFAR10(root='AIRecode/resnet18_on_imbalanced_dataset/dataset_imbalance', train=True,
+                                download=True, transform=transform)
+    test_data = torchvision.datasets.CIFAR10(root='AIRecode/resnet18_on_imbalanced_dataset/dataset_imbalance', train=False,
+                                download=True, transform=transform) # 这里运行好之后，可以把两处download=True改为False
+    
+    num_train = len(train_data)
+    print(num_train)
+    indices = list(range(num_train))
+    # random indices
+    np.random.shuffle(indices)
+    # the ratio of split
+    split = int(np.floor(valid_size * num_train))
+    # divide data to radin_data and valid_data
+    train_idx, valid_idx = indices[split:], indices[:split]
+
+    print(len(train_idx))
+    print(len(valid_idx))
+
+
+    # define samplers for obtaining training and validation batches
+    # 无放回地按照给定的索引列表采样样本元素
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+
+    # prepare data loaders (combine dataset and sampler)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+        sampler=train_sampler, num_workers=num_workers)
+    valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+        sampler=valid_sampler, num_workers=num_workers)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
+        num_workers=num_workers)
+
+    return train_loader,valid_loader,test_loader
---
+++ test.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from imbalance_cifar import read_dataset
+from models.ResNet import ResNet18
+# set device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+n_class = 10
+batch_size = 100
+train_loader,valid_loader,test_loader = read_dataset(batch_size=batch_size)
+model = ResNet18() # 得到预训练模型
+model.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+model.fc = torch.nn.Linear(512, n_class) # 将最后的全连接层修改
+# 载入权重
+model.load_state_dict(torch.load('/mnt/home/xuhan1/yangnan/AIRecode/resnet18_on_imbalanced_dataset/checkpoint/resnet18_cifar10_balance.pt'))
+model = model.to(device)
+
+total_sample = 0
+right_sample = 0
+
+correct_per_class = [0] * n_class
+total_per_class = [0] * n_class
+
+model.eval()  # 验证模型
+for data, target in test_loader:
+    data = data.to(device)
+    target = target.to(device)
+    # forward pass: compute predicted outputs by passing inputs to the model
+    output = model(data).to(device)
+    # convert output probabilities to predicted class(将输出概率转换为预测类)
+    _, pred = torch.max(output, 1)    
+    # compare predictions to true label(将预测与真实标签进行比较)
+    # correct_tensor = pred.eq(target.data.view_as(pred))
+    # # correct = np.squeeze(correct_tensor.to(device).numpy())
+    # total_sample += batch_size
+    # for i in correct_tensor:
+    #     if i:
+    #         right_sample += 1
+    for t, p in zip(target, pred):
+            total_per_class[t.item()] += 1
+            if t.item() == p.item():
+                correct_per_class[t.item()] += 1
+# print("Accuracy:",100*right_sample/total_sample,"%")
+
+
+accuracy_per_class = [100 * correct / total for correct, total in zip(correct_per_class, total_per_class)]
+
+# 打印每个类别的准确率
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')  # CIFAR-10 类别名称
+for i, class_name in enumerate(classes):
+    print(f"Accuracy of {class_name}: {accuracy_per_class[i]:.2f}%")
